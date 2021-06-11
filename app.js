23fefc55cf20006ad2246eedb3e90fc3df88cec3
grbix22 <grbesa391@gmail.com>
2021-06-11 20:49:03 +0200

    add

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var x = 5;
 
 if (typeof x !== "number") {
   console.log("Not a number!");
-} else if (x % 2 === 0 && x !== 0) {
+} else if (x === 0) {
+  console.log("Number is 0");
+} else if (x % 2 === 0) {
   console.log("Number is even!");
 } else if (x % 2 !== 0) {
   console.log("Number is odd");
-} else if (x === 0) {
-  console.log("Number is 0");
 }
